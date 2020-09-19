0cfce6a9e7e12eb4f6ebd3f9dd958dc0e963a166
Azeezah Muhammad <azeezahm14@gmail.com>
2020-09-19 16:28:28 -0400

    Animate dog on screen

--- renderer.js
+++ renderer.js
@@ -4,3 +4,20 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+var dog = document.getElementById('dog');
+
+const start = Date.now();  // timestamp in ms
+const ms_to_cross_screen = 5000;
+const fps = 60;
+const ms_per_frame = 1000/fps;
+var interval;
+
+interval = setInterval(() => {
+  let x = screen.width * (Date.now() - start) / ms_to_cross_screen;
+  dog.style.left = x + 'px';
+
+  if (x > screen.width) {
+    clearInterval(interval);
+  }
+}, ms_per_frame);
