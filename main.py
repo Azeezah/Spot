e727b6d5bffee5bf2cbe6e9faa2f5b8795a638db
Azeezah Muhammad <azeezahm14@gmail.com>
2020-09-19 17:50:31 -0400

    Add main.py with outline of app structure

---
+++ main.py
@@ -0,0 +1,32 @@
+def takeScreenshot():
+    pass
+
+def triggerLogin():
+    '''Trigger Auth0 and collect email address.'''
+    pass
+
+def reportDogSighting(userid):
+    '''Reports dog sighting to server.'''
+    pass
+
+def displayDogs(dogs):
+    '''
+    Send dogs to electron for display.
+    This can be asynchronous if we can remove the display dogs from the screenshot.
+    '''
+    pass
+
+def getFriendDogsSightings():
+    '''Requests list of friends' dog sightings from flask server.'''
+    pass
+
+def detectDog(imgFilename):
+    pass
+
+if __name__ == '__main__':
+    userid = triggerLogin()
+    while True:
+        if detectDog(takeScreenshot()):
+            reportDogSighting(userid)
+        dogs_of_friends = getFriendDogsSightings()
+        displayDogs(dogs_of_friends)
